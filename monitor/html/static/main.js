ff07acc6216aabe56d70d94379cf1e22926dea67
Fernando Constantino <const.fernando@gmail.com>
2021-09-16 02:34:03 -0300

    fixed countdown and refactored main js script

--- monitor/html/static/main.js
+++ monitor/html/static/main.js
@@ -1,31 +1,88 @@
-function updateData() {
-  console.log('Refreshing health data...')
-  fetch('/stats.txt')
+/**
+ * uServer Health
+ *
+ * Main page script
+ */
+
+const COUNTDOWN_REFRESH_MILLISECONDS = 1000;
+let countdownIntervalHandler = null;
+let secondsUntilNextRefresh = 0;
+
+/**
+ * Helper function to set the status container text
+ *
+ * @param text
+ */
+function setStatusText(text) {
+  window.document.getElementById('status').innerText = text;
+}
+
+/**
+ * Perform a refresh of the health data by fetching the stats file again
+ */
+function refreshData() {
+  const currentTimestamp = new Date().getTime();
+  console.log('Refreshing health data for timestamp ' + currentTimestamp);
+  setStatusText('Refreshing health data...');
+
+  fetch('/stats.txt?' + currentTimestamp)
     .then(response => response.text())
     .then((data) => {
+      setStatusText('Health data refreshed!')
       window.document.getElementById('heartbeatBox').innerText = data;
+      setCountdownOn();
     })
 }
 
+/**
+ * Return the selected amount of seconds between the data refresh
+ *
+ * @returns {number}
+ */
 function getRefreshSelectValue() {
-  return window.document.getElementById('refreshSecondsSelect').value;
+  return parseInt(window.document.getElementById('refreshSecondsSelect').value);
 }
 
-let secondsToRefresh = getRefreshSelectValue();
-
+/**
+ * Add the event listener to the select field to update the amount of seconds before refreshing the data again
+ */
 window.document.getElementById('refreshSecondsSelect').addEventListener('change', () => {
-  secondsToRefresh = getRefreshSelectValue();
+  secondsUntilNextRefresh = getRefreshSelectValue();
 });
 
-function updateTimer() {
-  if (secondsToRefresh <= 0) {
-    updateData()
-    secondsToRefresh = getRefreshSelectValue();
-  } else {
-    secondsToRefresh--;
-  }
+/**
+ * Timer callback function to compute the seconds left before refreshing data again
+ */
+function timerCallback() {
+  secondsUntilNextRefresh--;
 
-  window.document.getElementById('countdown').innerText = 'Data will be updated again in ' + secondsToRefresh + ' seconds...';
+  if (secondsUntilNextRefresh <= 0) {
+    setCountdownOff();
+    refreshData();
+    secondsUntilNextRefresh = getRefreshSelectValue();
+  } else {
+    const formattedTimeLeft = new Date(secondsUntilNextRefresh * 1000).toISOString().substr(11, 8);
+    setStatusText('Health data will be refreshed again in ' + formattedTimeLeft);
+  }
 }
 
-setInterval(updateTimer, 1000); // Time in milliseconds
+/**
+ * Set the timer event that will fire the callback function every n seconds (defined by a const)
+ */
+function setCountdownOn() {
+  countdownIntervalHandler = setInterval(timerCallback, COUNTDOWN_REFRESH_MILLISECONDS);
+}
+
+/**
+ * Reset (clear) the timer event that was calling the callback function
+ */
+function setCountdownOff() {
+  if (countdownIntervalHandler !== null) {
+    clearInterval(countdownIntervalHandler);
+  }
+}
+
+/**
+ * Main script
+ */
+setCountdownOn();
