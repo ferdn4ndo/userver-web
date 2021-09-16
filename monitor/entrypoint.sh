4bad475c15be5b34b46823b86d92c39edb3eaf14
Fernando Constantino <const.fernando@gmail.com>
2021-09-16 02:31:56 -0300

    review fixes

--- monitor/entrypoint.sh
+++ monitor/entrypoint.sh
@@ -2,5 +2,5 @@
 
 nginx
 
-echo "Starting the stats watcher fo every $REFRESH_EVERY_SECONDS second(s)"
+echo "Starting the stats watcher refreshing every $REFRESH_EVERY_SECONDS second(s)"
 nohup watch -n"$REFRESH_EVERY_SECONDS" 'sh /etc/monitor/refresh.sh' > /dev/null
